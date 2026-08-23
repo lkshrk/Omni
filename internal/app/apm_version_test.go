3654209a268a0534cf2d463e2fd123fc7f27869d
Lukas H <dev@harke.me>
2026-08-23 07:07:34 +0000

    chore: repin APM after CI boundary fix

--- internal/app/apm_version_test.go
+++ internal/app/apm_version_test.go
@@ -33,7 +33,7 @@ func TestParseAPMVersion(t *testing.T) {
 }
 
 func TestAPMVersionPin(t *testing.T) {
-	if apmVersionPin != "0.28.0+omni.5" || apmPackagePin != "git+https://github.com/lkshrk/apm.git@a041fd8f6ffb8b01e3d8e366e483b3297662559b" {
+	if apmVersionPin != "0.28.0+omni.5" || apmPackagePin != "git+https://github.com/lkshrk/apm.git@f7bc3475ae77895c3179f1e752edc649465e4b36" {
 		t.Fatalf("unexpected APM pins: version=%q package=%q", apmVersionPin, apmPackagePin)
 	}
 	for _, tt := range []struct {
